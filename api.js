649d01c7675e04a19820c52902d5ad76672f0c9e
Hack Reactor Students <students@hackreactor.com>
2013-12-20 15:25:21 -0800

    feat: update fitbit OAuth

--- api.js
+++ api.js
@@ -1,8 +1,30 @@
-var express = require('express');
+var express = require('express'),
+    fitbit = ('fitbit-js');
+
 var api = express();
 
-api.get('/', function(req, res) {
-  res.send('wassup');
+
+var token;
+app.get('/auth/fitbit', function (req, res) {
+  fitbitClient.getAccessToken(req, res, function (error, newToken) {
+    if(newToken) {
+      token = newToken;
+    }
+  });
+});
+
+app.get('/getStuff', function (req, res) {
+  fitbitClient.apiCall('GET', '/user/-/activities/date/2013-12-19.json',
+    {token: {oauth_token_secret: token.oauth_token_secret,
+             oauth_token: token.oauth_token}},
+    function(err, resp, json) {
+      if (err) return res.send(err, 500);
+      res.json(json);
+  });
+});
+
+app.get('/cookie', function(req, res) {
+  res.send('wahoo!');
 });
 
 var port = 8000;
