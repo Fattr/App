2e74d541d0f69c293b33e23d55a2fc0f65ccadd4
Mehul Patel <mehul8@gmail.com>
2013-12-21 14:23:10 -0800

    added dependencies to bower.json and fixed spacing issue with chart

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,6 @@ angular.module('mean.system', []);
 angular.module('mean.articles', []);
 angular.module('mean.dashboard', []);
 angular.module('nvd3ChartDirectives', []);
+// You have to download the directive manually since it's not available on bower
+// http://cmaurer.github.io/angularjs-nvd3-directives/stacked.area.chart.html
+// rename the unzipped folder to nvd3-directive and put it in the lib folder
