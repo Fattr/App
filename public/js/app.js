79107e4dc15049773f85b56cdc3617fba4d25b41
Mehul Patel <mehul8@gmail.com>
2013-12-21 14:05:10 -0800

    implemented a chart with dummy data.

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.dashboard']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ngRoute', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.articles', 'mean.dashboard', 'nvd3ChartDirectives']);
 
 angular.module('mean.system', []);
 angular.module('mean.articles', []);
 angular.module('mean.dashboard', []);
-
+angular.module('nvd3ChartDirectives', []);
