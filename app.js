9457c9b77cb68d42c6adf2d76bc7eef8d6272f45
Hendrixer <willscottmoss@gmail.com>
2013-12-26 12:55:05 -0800

    fix: add facebook Oauth with passport middleware

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+var config  = require('./oauth');
 var express = require('express');
 var app			= express();
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
+
+// config
+passport.use(new FacebookStrategy({
+ clientID: config.facebook.clientID,
+ clientSecret: config.facebook.clientSecret,
+ callbackURL: config.facebook.callbackURL
+},
+function(accessToken, refreshToken, profile, done) {
+ process.nextTick(function () {
+   return done(null, profile);
+ });
+}
+));
 
 
 app.configure(function(){ 
@@ -7,8 +30,12 @@ app.configure(function(){
   
   app.use(express.favicon());
   app.use(express.logger('dev'));
+  app.use(express.cookieParser());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.session({secret: 'super_fit'}));
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 
@@ -22,6 +49,18 @@ app.get('/', function(req, res) {
 	res.sendfile(__dirname + '/public/index.html');
 });
 
+app.get('/auth/facebook',
+  passport.authenticate('facebook'),
+  function(req, res){
+  }
+);
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
 
 
 
---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+	facebook: {
+		clientID: '292173437605995',
+		clientSecret: 'd471dc58c7c519ded6d72aef4321495c',
+		callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback'
+	}
+};
+
+module.exports = ids;
