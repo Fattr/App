c5fecbe9085e27f7e14eb2cdec9f1f94baa6c7d2
wuwoot <dwu207@gmail.com>
2013-12-26 23:16:27 -0800

    removed URL from the callbackURL string

--- oauth.js
+++ oauth.js
@@ -7,7 +7,7 @@ var ids = {
 	fitbit: {
 		consumerKey: '6b8b28e0569a422e97a70b5ca671df32',
 		consumerSecret: 'b351c1fea45d48ed9955a518f4e30e72',
-		callbackURL: 'http://127.0.0.1:3000/auth/fitbit/callbackURL'
+		callbackURL: 'http://127.0.0.1:3000/auth/fitbit/callback'
 	}
 };
 
