09acef48e202c2fe1785ae6a59107c18d8a793aa
Scott Moss <Hendrixer@users.noreply.github.com>
2014-01-02 18:45:07 -0800

    Update worker to work

--- worker.js
+++ worker.js
@@ -1,85 +1,45 @@
-/**
- * UPDATES DB WITH PREVIOUS DAY'S FITBIT DATA
- */
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/fittr');
+var User = require('./app/models/users.js');
+var Steps = require('./app/models/fbSteps.js');
+var config = require('./config/auth.js').fitbit;
+var fitbitClient = require('fitbit-js')(config.consumerKey, config.consumerSecret, config.callbackURL);
 
-var mongoose      = require('mongoose'),
-    db            = mongoose.connect('mongodb://localhost/fittr-dev'),
-    User          = require('./app/models/users.js'),
-    Steps         = require('./app/models/fbSteps.js'),
-    config        = require('./config/auth');
-    fitbitClient  = require('fitbit-js')(config.fitbit.consumerKey, config.fitbit.consumerSecret);
-
-
-var fetch = function() {
-
-  function userData(userCred) {
-    // get yesterday's date for the fitbit API call
-    var yesterday = function() {
-      var date = new Date();
-      date.setDate(date.getDate() - 1);
-      return date;
-    }();
-
-    // fitbit json response comes with an activities array
-    // that contains an obj with type of activity and dist
-    // following func is a summation of all those dists
-    var totalDist = function(userAct) {
-      var totalDist = 0;
-      var distances = user.summary.distances;
-      for (var i = 0; i < distances.length; i++) {
-        totalDist += distances[i].distance;
-      }
-      return totalDist;
+// =================================
+// get all users fibit access tokens
+// retrieve prior days info and save
+// to database
+// =================================
+var updateUsers = function(users) {
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    var token = {
+      oauth_token: user.fitbit.token,
+      oauth_token_secret: user.fitbit.tokenSecret
     };
-
-    // updates db with formatted user activity
-    function updateDB(userAct) {
-      var dailyAct = new Steps({
-        user:         userAct.id,
-        steps:        userAct.summary.steps,
-        distance:     userAct.distance,
-        seditaryMin:  userAct.summary.sedentaryMinutes,
-        fairlyActMin: userAct.summary.fairlyActiveMinutes,
-        veryActMin:   userAct.summary.veryActiveMinutes
+    console.log('token', token);
+    fitbitClient.apiCall('GET', '/user/-/activities/date/2013-11-29.json',
+      {token: token},
+      function(err, resp, json) {
+        console.log('api call');
+        if(err) {
+          console.log('err', err);
+          return err;
+        }
+        console.log(json);
       });
-      dailyAct.save(function(err) {
-        if (err) throw err;
-      });
-    }
-
-    // get JSON blob of daily user activity
-    fitbitClient.apiCall(
-      'GET',
-      '/user/-/activities/date/' + yesterday + '.json',
-      userCred.token,
-      function(err, resp, userAct) {
-        // if (err) return res.send(err, 500);
-
-        userAct.id = userCred.id;
-        userAct.distance = totalDist(userAct);
-
-        // send the userData to the DB
-        updateDB(userAct);
-    });
   }
-
-  // SET OFF
-  User.find({}, function(err, users) {
-    for (var i = 0; i < users.length; i++) {
-      var user = users[i];
-      // create user credentials (id, tokens)
-      var userCred = {};
-      userCred.id = user.fitbit.id;
-      userCred.token = {
-        oauth_token_secret: user.fitbit.tokenSecret,
-        oauth_token: user.fitbit.token
-      };
-      userData(userCred);
-    }
-  });
 };
 
-/** 
- * invoke the worker to update data for all users
- */
-fetch();
+User.find({}, function(err, users) {
+  if(err) {
+    console.log('could not find users', err);
+    return err;
+  }
+
+  if(users) {
+    updateUsers(users);
+  }
+})
+
+
