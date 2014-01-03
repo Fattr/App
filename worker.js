c7672231268eb448030091ca79d20bedd32617dc
wuwoot <dwu207@gmail.com>
2014-01-03 00:13:06 -0800

    fixed typos in fbStepsSchema; reformatted entry-data in worker.js

--- worker.js
+++ worker.js
@@ -1,45 +1,85 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fittr');
-var User = require('./app/models/users.js');
-var Steps = require('./app/models/fbSteps.js');
-var config = require('./config/auth.js').fitbit;
-var fitbitClient = require('fitbit-js')(config.consumerKey, config.consumerSecret, config.callbackURL);
+var mongoose      = require('mongoose');
+    db            = mongoose.connect('mongodb://localhost/fittr');
+    User          = require('./app/models/users.js');
+    Steps         = require('./app/models/fbSteps.js');
+    config        = require('./config/auth.js').fitbit;
+    fitbitClient  = require('fitbit-js')(config.consumerKey, config.consumerSecret, config.callbackURL);
 
 // =================================
 // get all users fibit access tokens
 // retrieve prior days info and save
 // to database
 // =================================
-var updateUsers = function(users) {
+var yesterday = function() {
+  var date = new Date();
+  date.setDate(date.getDate() - 1);
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  if (month < 10) {
+    month = 0 + String(month);
+  }
+  var day = date.getDate();
+  if (day < 10) {
+    day = 0 + String(day);
+  }
+  return '' + year + '-' + month + '-' + day;
+}();
+
+var updateDb = function(userActivities) {
+  var dailyActivities = new Steps({
+    user:             userActivities.id,
+    date:             userActivities.date,
+    steps:            userActivities.summary.steps,
+    distances:        userActivities.distances,
+    caloriesBurned:   userActivities.caloriesOut,
+    sedentaryMins:    userActivities.summary.sedentaryMinutes,
+    fairlyActMins:    userActivities.summary.fairlyActiveMinutes,
+    veryActMins:      userActivities.summary.veryActiveMinutes
+  });
+
+  dailyActivities.save(function(err, activities, numAffected) {
+    if (err) throw err;
+    else {
+      console.log('activities', activities);
+      console.log('number affected', numAffected);
+    }
+  });
+};
+
+var getActivities = function(users) {
   for (var i = 0; i < users.length; i++) {
     var user = users[i];
     var token = {
       oauth_token: user.fitbit.token,
       oauth_token_secret: user.fitbit.tokenSecret
     };
-    console.log('token', token);
-    fitbitClient.apiCall('GET', '/user/-/activities/date/2013-11-29.json',
+    // console.log('token', token);
+    fitbitClient.apiCall(
+      'GET', '/user/-/activities/date/' + yesterday +'.json',
       {token: token},
-      function(err, resp, json) {
-        console.log('api call');
-        if(err) {
-          console.log('err', err);
-          return err;
+      function(err, resp, userActivities) {
+        if (err) throw err;
+        else {
+          userActivities.id = user._id;
+          userActivities.date = yesterday;
+          console.log(userActivities);
+          // updateDb(userActivities);
         }
-        console.log(json);
-      });
+      }
+    );
   }
 };
 
+// Grab all users from DB
 User.find({}, function(err, users) {
-  if(err) {
+  if (err) {
     console.log('could not find users', err);
     return err;
   }
 
-  if(users) {
-    updateUsers(users);
+  if (users) {
+    getActivities(users);
   }
-})
+});
 
 
