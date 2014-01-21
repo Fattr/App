7b0d7bf07d0bff68c712c3933fdd535116aa1641
sudostack <dwu207@gmail.com>
2014-01-21 11:54:56 -0800

    side menu works

--- www/js/src/controllers/mainController.js
+++ www/js/src/controllers/mainController.js
@@ -1,4 +1,9 @@
 angular.module('fittr.controllers')
 
 .controller('MainController', function($scope) {
+
+  $scope.toggleMenu = function() {
+    $scope.sideMenuController.toggleLeft();
+  };
+
 });
